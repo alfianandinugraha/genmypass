da6c30563679294a1668df158538c9d53a7393de
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-02-27 01:48:32 +0700

    feat(layout): init

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./style/root.css"
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+})
 
 console.log("Hello world !")
